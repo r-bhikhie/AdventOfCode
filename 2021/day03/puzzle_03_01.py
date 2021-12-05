417d463bf087a5f92ea3c4d9651e6a9a9f3c02ad
Ravi Bhikhie <r.bhikhie@gmail.com>
2021-12-05 22:52:17 +0100

    Solve day 03, part 1

--- 2021/day03/puzzle_03_01.py
+++ 2021/day03/puzzle_03_01.py
@@ -6,17 +6,36 @@
 # Q: Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together.
 # What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
 
+# Observations: inputs are 12 digits wide. Max value eps/gam is 2^12 or 4096.
+
 def main():
     
-    answer = 0
-    gamma = 0
-    epsilon = 0
+    gamma = ""
+    epsilon = ""
+    zero_counting_box = [0,0,0,0,0,0,0,0,0,0,0,0]
+    one_counting_box = [0,0,0,0,0,0,0,0,0,0,0,0]
     
-    with open('input_02.txt', "r", encoding='utf-8') as f:
+    with open('input_03.txt', "r", encoding='utf-8') as f:
         for line in f:
-            pass
+            line = line.strip()
+            
+            for idx, input_bit in enumerate(line):
+                if input_bit == "0":
+                    zero_counting_box[idx] += 1
+                if input_bit == "1":
+                    one_counting_box[idx] += 1
+    
+    for idx,bit in enumerate(zero_counting_box):
+        if zero_counting_box[idx] > one_counting_box[idx]:
+            gamma += "0"
+            epsilon += "1"
+        else:
+            gamma += "1"
+            epsilon += "0"
+    
+    answer = int(gamma,2)*int(epsilon,2)
 
-    print(f'Episilon: {epsilon}, Gamma: {gamma}.')
+    print(f'Epsilon: {epsilon}, Gamma: {gamma}.')
     print(f'Final answer: {answer}')
 
 if __name__ == '__main__':
